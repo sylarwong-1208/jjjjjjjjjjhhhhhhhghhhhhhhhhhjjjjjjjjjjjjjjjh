fe9de782f2939c97f00eccd85bf57e362277ec8d
sylarwong-1208 <119169132+sylarwong-1208@users.noreply.github.com>
2022-12-01 00:31:16 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.showIcon(IconNames.SmallHeart)
+    }
+    basic.clearScreen()
+})
